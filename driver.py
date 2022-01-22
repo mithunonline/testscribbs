89715a705bc19396e19987f6940cc1617abfec4d
mithun <mithunonmail@gmail.com>
2022-01-22 20:30:14 +0530

    firstcommit

---
+++ driver.py
@@ -0,0 +1,10 @@
+class DriverProgram:
+    def my_function(self):
+        print('inside my function')
+
+
+
+if __name__ == '__main__':
+    print('entering main method')
+    driver = DriverProgram()
+    driver.my_function()
